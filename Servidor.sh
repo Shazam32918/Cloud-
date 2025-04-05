afb606ae0a52025f563ab99e40120f5a3f96cb4b
Shazam32918 <gomezgonzalezandressimonei@gmail.com>
2025-04-04 20:21:11 -0400

    Create Servidor.sh

---
+++ Servidor.sh
@@ -0,0 +1,5 @@
+wget https://download.oracle.com/java/21/latest/jdk-21_linux-x64_bin.deb
+sudo dpkg -i jdk-21_linux-x64_bin.deb
+mkdir servidor_por_andrew
+wget https://api.papermc.io/v2/projects/paper/versions/1.21.4/builds/222/downloads/paper-1.21.4-222.jar
+mv paper-1.21.4-22.jar server.jar
